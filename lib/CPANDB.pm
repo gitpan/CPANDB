769ebc02a97f59c799f97f071779ebdebeab061d
Adam Kennedy <adamk@cpan.org>
2009-08-12 17:07:19 +0000

    Import of ADAMK/CPANDB-0.06 from CPAN.
    gitpan-cpan-distribution: CPANDB
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         ADAMK/CPANDB-0.06.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

--- lib/CPANDB.pm
+++ lib/CPANDB.pm
@@ -3,11 +3,12 @@ package CPANDB;
 use 5.008005;
 use strict;
 use warnings;
+use IO::File             ();
 use Params::Util         ();
 use ORLite::Mirror       ();
 use CPANDB::Distribution ();
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 sub import {
 	my $class  = shift;
@@ -47,6 +48,43 @@ sub graph {
 		$graph->add_edge( $edge->distribution => $edge->dependency );
 	}
 	return $graph;
+}
+
+sub easy {
+	require Graph::Easy;
+	my $class = shift;
+	my $graph = Graph::Easy->new;
+	foreach my $vertex ( CPANDB::Distribution->select ) {
+		$graph->add_vertex( $vertex->distribution );
+	}
+	foreach my $edge ( CPANDB::Dependency->select ) {
+		$graph->add_edge( $edge->distribution => $edge->dependency );
+	}
+	return $graph;	
+}
+
+sub xgmml {
+	require Graph::XGMML;
+	my $class = shift;
+	my $graph = Graph::XGMML->new( directed => 1, @_ );
+	foreach my $vertex ( CPANDB::Distribution->select ) {
+		$graph->add_vertex( $vertex->distribution );
+	}
+	foreach my $edge ( CPANDB::Dependency->select ) {
+		$graph->add_edge( $edge->distribution => $edge->dependency );
+	}
+	$graph->end;
+	return 1;
+}
+
+sub csv {
+	my $class = shift;
+	my $file  = shift;
+	my $csv   = IO::File->new($file, 'w');
+	foreach my $edge ( CPANDB::Dependency->select ) {
+		$csv->print( $edge->distribution . "\t" . $edge->dependency . "\n" );
+	}
+	$csv->close;
 }
 
 1;
--- lib/CPANDB/Distribution.pm
+++ lib/CPANDB/Distribution.pm
@@ -4,7 +4,7 @@ use 5.008005;
 use strict;
 use warnings;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 
 
@@ -13,21 +13,14 @@ our $VERSION = '0.05';
 ######################################################################
 # Graph Integration
 
-use constant NODE_METHOD => {
-	'Graph::Directed' => 'add_vertex',
-	'Graph::Easy'     => 'add_node',
-	'GraphViz'        => 'add_node',
-};
-
-use constant EDGE_METHOD => {
-	'Graph::Directed' => 'add_edge',
-	'Graph::Easy'     => 'add_edge',
-	'GraphViz'        => 'add_edge',
-};
-
 sub dependency_graph {
 	require Graph::Directed;
 	shift->_dependency( _class => 'Graph::Directed', @_ );
+}
+
+sub dependants_graph {
+	require Graph::Directed;
+	shift->_dependants( _class => 'Graph::Directed', @_ );
 }
 
 sub dependency_easy {
@@ -35,9 +28,29 @@ sub dependency_easy {
 	shift->_dependency( _class => 'Graph::Easy', @_ );
 }
 
+sub dependants_easy {
+	require Graph::Easy;
+	shift->_dependants( _class => 'Graph::Easy', @_ );
+}
+
 sub dependency_graphviz {
 	require GraphViz;
 	shift->_dependency( _class => 'GraphViz', @_ );
+}
+
+sub dependants_graphviz {
+	require GraphViz;
+	shift->_dependants( _class => 'GraphViz', @_ );
+}
+
+sub dependency_xgmml {
+	require Graph::XGMML;
+	shift->_dependency( _class => 'Graph::XGMML', @_ );
+}
+
+sub dependants_xgmml {
+	require Graph::XGMML;
+	shift->_dependants( _class => 'Graph::XGMML', @_ );
 }
 
 sub _dependency {
@@ -48,8 +61,9 @@ sub _dependency {
 	my $perl     = delete $param{perl};
 
 	# Prepare support values for the algorithm
-	my $add_node  = NODE_METHOD->{$class};
-	my $add_edge  = EDGE_METHOD->{$class};
+	my $add_node  = $class->can('add_vertex')
+		? 'add_vertex'
+		: 'add_node';
 	my $sql_where = 'where distribution = ?';
 	my @sql_param = ();
 	if ( $phase ) {
@@ -81,7 +95,57 @@ sub _dependency {
 			$sql_where, $name, @sql_param,
 		);
 		foreach my $dep ( @deps ) {
-			$graph->$add_edge( $name => $dep );
+			$graph->add_edge( $name => $dep );
+		}
+
+		# Push the new ones to the list
+		push @todo, grep { not $seen{$_}++ } @deps;
+	}
+
+	return $graph;
+}
+
+sub _dependants {
+	my $self     = shift;
+	my %param    = @_;
+	my $class    = delete $param{_class};
+	my $phase    = delete $param{phase};
+	my $perl     = delete $param{perl};
+
+	# Prepare support values for the algorithm
+	my $add_node  = $class->can('add_vertex') ? 'add_vertex' : 'add_node';
+	my $sql_where = 'where dependency = ?';
+	my @sql_param = ();
+	if ( $phase ) {
+		$sql_where .= ' and phase = ?';
+		push @sql_param, $phase;
+	}
+	if ( $perl ) {
+		$sql_where .= ' and ( core is null or core >= ? )';
+		push @sql_param, $perl;
+	}
+
+	# Pass any remaining params to the graph constructor
+	my $graph = $class->new( %param );
+
+	# Fill the graph via simple list recursion
+	my @todo = ( $self->distribution );
+	my %seen = ( $self->distribution => 1 );
+	while ( @todo ) {
+		my $name = shift @todo;
+		$graph->$add_node( $name );
+
+		# Find the distinct dependencies for this node
+		my %edge = ();
+		my @deps = grep {
+			not $edge{$_}++
+		} map {
+			$_->dependency
+		} CPANDB::Dependency->select(
+			$sql_where, $name, @sql_param,
+		);
+		foreach my $dep ( @deps ) {
+			$graph->add_edge( $name => $dep );
 		}
 
 		# Push the new ones to the list
